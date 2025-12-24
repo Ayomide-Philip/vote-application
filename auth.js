4d5643d77351bb781dd53ffbf7c08aca3c809bfd
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-24 05:05:35 +0100

    Add Google authentication support to NextAuth configuration

--- auth.js
+++ auth.js
@@ -1,5 +1,19 @@
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import {
+  AUTH_GOOGLE_ID,
+  AUTH_GOOGLE_SECRET,
+  NEXTAUTH_SECRET,
+} from "./libs/config/configuration";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Google({
+      clientId: AUTH_GOOGLE_ID,
+      clientSecret: AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  secret: NEXTAUTH_SECRET,
+  callbacks: {},
+  session: "jwt",
 });
--- libs/config/configuration.js
+++ libs/config/configuration.js
@@ -1,1 +1,6 @@
-export const { NEXTAUTH_SECRET, MONGODB_URI } = process.env;
+export const {
+  NEXTAUTH_SECRET,
+  MONGODB_URI,
+  AUTH_GOOGLE_ID,
+  AUTH_GOOGLE_SECRET,
+} = process.env;
