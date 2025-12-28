45363ff31774b10ce57f27d11579bcbabbd598e3
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2025-12-28 23:42:22 +0100

    feat: define Polls schema with timestamps for tracking creation and updates

--- libs/models/polls.models.js
+++ libs/models/polls.models.js
@@ -1,1 +1,17 @@
-import {} from "mongoose";
+import { model, models, Schema } from "mongoose";
+
+const pollsModels = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Polls = models.Polls || model("Polls", pollsModels);
+export default Polls;
