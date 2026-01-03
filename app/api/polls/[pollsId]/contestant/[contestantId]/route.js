5d4a12e4e8be294d4b8118388ebcf1d96f8ecbd9
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-01-03 21:41:32 +0100

    feat: implement GET endpoint for retrieving a specific contestant with error handling

--- app/api/polls/[pollsId]/contestant/[contestantId]/route.js
+++ app/api/polls/[pollsId]/contestant/[contestantId]/route.js
@@ -1,5 +1,62 @@
 import { NextResponse } from "next/server";
+import { connectDatabase } from "@/libs/connectdatabase";
+import Polls from "@/libs/models/polls.models";
+import Contestant from "@/libs/models/contestant.models";
+import User from "@/libs/models/user.models";
 
 export async function GET(req, { params }) {
-  return NextResponse.json({ message: "GET a particular contestant" });
+  const { contestantId, pollsId } = await params;
+  // if there is no poll id
+  if (!pollsId) {
+    return NextResponse.json(
+      { error: "Poll Id is not defined" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // if there is no contestantId
+  if (!contestantId) {
+    return NextResponse.json(
+      { error: "Contestant Id is not defined" },
+      {
+        status: 400,
+      }
+    );
+  }
+  // check the database
+  try {
+    await connectDatabase();
+    // check if the poll exist
+    const poll = await Polls.findById(pollsId);
+    if (!poll) {
+      return NextResponse.json(
+        { error: "Poll does not exist" },
+        {
+          status: 400,
+        }
+      );
+    }
+    // check if the contestant exist
+    const contestants = await Contestant.findOne({
+      _id: contestantId,
+      pollId: pollsId,
+    })
+      .populate("candidates.userId", "name image department faculty")
+      .populate("pollId", "startDate endDate title description");
+    return NextResponse.json(
+      { contestants },
+      {
+        status: 200,
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json(
+      { error: "Network Error" },
+      {
+        status: 400,
+      }
+    );
+  }
 }
