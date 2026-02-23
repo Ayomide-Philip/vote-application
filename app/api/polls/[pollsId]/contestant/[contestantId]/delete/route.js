b796d182be5434c012af99b91f363e2fe67869a9
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-02-23 14:26:30 +0100

    updating my route.js

--- app/api/polls/[pollsId]/contestant/[contestantId]/delete/route.js
+++ app/api/polls/[pollsId]/contestant/[contestantId]/delete/route.js
@@ -2,10 +2,27 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(req, { params }) {
   const { pollsId, contestantId } = await params;
-  return NextResponse.json(
-    { message: "DELETING my contestant", pollsId, contestantId },
-    {
-      status: 200,
-    },
-  );
+  if (!pollsId || !contestantId) {
+    return NextResponse.json(
+      { error: "Invalid Parameters" },
+      {
+        status: 400,
+      },
+    );
+  }
+  try {
+    return NextResponse.json(
+      { message: "DELETING my contestant", pollsId, contestantId },
+      {
+        status: 200,
+      },
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { error: "Unable to delete contestant position" },
+      {
+        status: 400,
+      },
+    );
+  }
 }
