42a58d17c494c0ce3a86a9f8d9470f86d670024e
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-12 21:53:24 +0100

    refactor(layout.js): remove optional polling code and delete unused CORS and polls routes

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -7,19 +7,6 @@ import { redirect } from "next/navigation";
 export default async function Layout({ children }) {
   const session = await auth();
   if (!session?.user && !session) return redirect("/");
-
-  // this line of code is optional
-  // setInterval(async () => {
-  //   const request = await fetch(`${BASE_URL}/api/cors/polls`, {
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       Cookie: (await cookies()).toString(),
-  //     },
-  //   });
-  //   const response = await request.json();
-  //   console.log(response);
-  // }, 30000);
-
   return (
     <>
       <DashboardNavigations session={session}>{children} </DashboardNavigations>
