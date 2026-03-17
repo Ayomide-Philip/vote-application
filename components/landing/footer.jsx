1e7b4cc855718a57b86d23804e452cbd1866d727
Ayomide Philip <a.areo.csc2023064@student.oauife.edu.ng>
2026-03-17 14:29:10 +0100

    fix(footer.jsx): comment out unused SVG element in footer component

--- components/landing/footer.jsx
+++ components/landing/footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="relative text-zinc-500 bg-zinc-50 dark:bg-zinc-800 dark:text-zinc-400 border-t border-zinc-200 dark:border-zinc-700">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row justify-between flex-col">
         <a className="flex title-font font-medium items-center md:justify-start justify-center text-zinc-900 dark:text-zinc-100">
-          <svg
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             stroke="currentColor"
@@ -16,7 +16,7 @@ export default function Footer() {
             viewBox="0 0 24 24"
           >
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          </svg> */}
           <span className="ml-3 text-xl">Ballot Right</span>
         </a>
         <p className="text-sm text-zinc-500 dark:text-zinc-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-zinc-200 dark:sm:border-zinc-700 sm:py-2 sm:mt-0 mt-4">
